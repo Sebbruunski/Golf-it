6193f810a0ddcdfc92fa4c075cac9a92a608d257
axel0591 <axelbartholin@gmail.com>
2023-10-09 10:31:08 +0200

    arrows

--- sketch.js
+++ sketch.js
@@ -17,30 +17,33 @@ function setup() {
     {form:"cir",x:3*width/4+3*pindMidt/4,y:height-25,d:15,col:[255],speed:0,dir:0}
   ]
   skyd = false 
-  ref=ball[0]
+  ref=0
 }
 
 function draw() {
   if (state == "play"){
+    
     ball[0].x+=cos(ball[0].dir)*ball[0].speed
     ball[1].x+=cos(ball[1].dir)*ball[1].speed
     ball[0].y+=sin(ball[0].dir)*ball[0].speed
     ball[1].y+=sin(ball[1].dir)*ball[1].speed
-    if(mouseIsPressed & skyd == false){
-      if(sqrt((mouseX-ball[0].x)**2+(mouseY-ball[0].y)**2)<=(ball[0].d*0.5)){
-        ref=ball[0]
-        ball[0].col=[255,0,0]
-        ball[1].col=[255]
-      }else if(sqrt((mouseX-ball[1].x)**2+(mouseY-ball[1].y)**2)<=(ball[1].d*0.5)){
-        ref=ball[1]
-        ball[1].col=[255,0,0]
-        ball[0].col=[255]
-      }else{
-      skyd = true
-      ball[0].dir =atan2((ref.y-mouseY),(ref.x-mouseX))
-      ball[1].dir =atan2((ref.y-mouseY),(ref.x-mouseX))
-      ball[1].speed =4
-      ball[0].speed =4
+    if(skyd == false){
+      ball[0].dir =atan2((ball[ref].y-mouseY),(ball[ref].x-mouseX))
+      ball[1].dir =atan2((ball[ref].y-mouseY),(ball[ref].x-mouseX))
+      if(mouseIsPressed){
+        if(sqrt((mouseX-ball[0].x)**2+(mouseY-ball[0].y)**2)<=(ball[0].d*0.5)){
+          ref=0
+          ball[0].col=[255,0,0]
+          ball[1].col=[255]
+        }else if(sqrt((mouseX-ball[1].x)**2+(mouseY-ball[1].y)**2)<=(ball[1].d*0.5)){
+          ref=1
+          ball[1].col=[255,0,0]
+          ball[0].col=[255]
+        }else{
+        skyd = true
+        ball[1].speed =4
+        ball[0].speed =4
+        }
       }
     }
     if(ball[1].speed>0.1 &ball[0].speed>0.1){
@@ -53,7 +56,7 @@ function draw() {
       skyd =false
     }
   }
-  
+  strokeWeight(1)
   Kollison(ball,bane,hul)
   noStroke()
   Grid(40)
@@ -61,6 +64,21 @@ function draw() {
   Tegn(ball)
   Tegn(hul)
   Tegn(bane)
+  //tegn pil
+  if(skyd==false){
+    strokeWeight(5)
+    stroke(255,150,50)
+    fill(255,150,50)
+    endX=ball[ref].x+cos(ball[ref].dir)*50
+    endY=ball[ref].y+sin(ball[ref].dir)*50
+    line(ball[ref].x,ball[ref].y,endX,endY)
+    triangle(endX,endY,endX-cos(ball[ref].dir+120)*10,endY-sin(ball[ref].dir+120)*10,endX-cos(ball[ref].dir-120)*10,endY-sin(ball[ref].dir-120)*10)
+    strokeWeight(2)
+    endX=ball[(ref+1)%2].x+cos(ball[(ref+1)%2].dir)*50
+    endY=ball[(ref+1)%2].y+sin(ball[(ref+1)%2].dir)*50
+    line(ball[(ref+1)%2].x,ball[(ref+1)%2].y,endX,endY)
+    triangle(endX,endY,endX-cos(ball[(ref+1)%2].dir+120)*10,endY-sin(ball[(ref+1)%2].dir+120)*10,endX-cos(ball[(ref+1)%2].dir-120)*10,endY-sin(ball[(ref+1)%2].dir-120)*10)
+  }
 }
 
 function Tegn(liste){
@@ -110,8 +128,8 @@ function Kollison(Spiller,bane,Hul){
     for(let j = 0;j<Hul.length;j++){
       if(Hul[j].d/2+Spiller[i].d/2>sqrt((Spiller[i].x-Hul[j].x)**2+(Spiller[i].y-Hul[j].y)**2)){
         Spiller[i].d=0
-        ref=Spiller[(i+1)%2]
-        Spiller[(i+1)%2].col=[255,0,0]
+        ref=(i+1)%2
+        Spiller[ref].col=[255,0,0]
       }
     }
   } 
